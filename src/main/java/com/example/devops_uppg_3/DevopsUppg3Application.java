275a504a563c8a8c79fc54a006393640769f7018
Emma Vainio <emmavainio@live.se>
2023-09-27 13:41:46 +0200

    Initial commit

---
+++ src/main/java/com/example/devops_uppg_3/DevopsUppg3Application.java
@@ -0,0 +1,13 @@
+package com.example.devops_uppg_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsUppg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsUppg3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devops_uppg_3/DevopsUppg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_uppg_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsUppg3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
